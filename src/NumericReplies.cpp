9b6d29a984b4976fae284665c352971cd5bf5654
sichoi42 <42.4.sichoi@gmail.com>
2022-12-09 17:52:49 +0900

    :loud_sound: update numericReplies #7
    update numericReplies #7

--- src/NumericReplies.cpp
+++ src/NumericReplies.cpp
@@ -1,6 +1,6 @@
 #include "Utils.hpp"
 
-// 421     
+// 421
 std::string ERR_UNKNOWNCOMMAND(const std::string &command) {
 	return command + " :Unknown command" ;
 }
@@ -13,4 +13,16 @@ std::string ERR_NEEDMOREPARAMS(const std::string &command) {
 // 462
 std::string ERR_ALREADYREGISTRED(void) {
 	return ":You may not reregister" ;
+}
+
+std::string ERR_NONICKNAMEGIVEN(void) {
+	return ":No nickname given" ;
+}
+
+std::string ERR_ERRONEUSNICKNAME(const std::string &nickname) {
+	return nickname + " :Erroneus nickname" ;
+}
+
+std::string ERR_NICKNAMEINUSE(const std::string &nickname) {
+	return nickname + " :Nickname is already in use" ;
 }
