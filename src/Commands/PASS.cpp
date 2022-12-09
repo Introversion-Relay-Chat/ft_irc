94de3c74929a2b14f90719d916580fcc19e658f0
sichoi42 <42.4.sichoi@gmail.com>
2022-12-09 17:51:57 +0900

    :memo: update error response on PASS #7
    update error response on PASS #7

--- src/Commands/PASS.cpp
+++ src/Commands/PASS.cpp
@@ -5,12 +5,14 @@ std::string PASS(const Message &message, User *sender)
 	std::string sender_prefix = sender->getServerPrefix();
 	std::string	target = sender->getNickname();
 
-	if (message.middle.size() < 1)
+	if (message.middle.size() < 1) {
 		return join(sender_prefix, "461", target, ERR_NEEDMOREPARAMS(message.command));
-	if (sender->getStatus() != PASSWORD)
+	}
+	if (sender->getStatus() != NEED_PASSWORD) {
 		return join(sender_prefix, "462", target, ERR_ALREADYREGISTRED());
-
-	if (sender->getServer()->getPassword() == message.middle[0])
-		sender->setStatus(REGISTER);
+	}
+	if (sender->getServer()->getPassword() == message.middle[0]) {
+		sender->setStatus(NEED_NICKNAME);
+	}
 	return std::string();
 }
