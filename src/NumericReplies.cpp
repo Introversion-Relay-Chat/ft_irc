05b0e52bfc98438d544798871a8ef7ed73cd4596
Donghyun Kim <donghyun@c10r2s2.42seoul.kr>
2022-12-09 17:11:07 +0900

    fix NumericReplies to cover all replies

--- src/NumericReplies.cpp
+++ src/NumericReplies.cpp
@@ -15,14 +15,14 @@ std::string ERR_ALREADYREGISTRED(void) {
 	return ":You may not reregister" ;
 }
 
-std::string NumericReply(std::string sender_prefix, std::string code, std::string target, const Message &message) {
+std::string NumericReply(std::string sender_prefix, std::string code, std::string target, std::string message) {
 	switch (atoi(code.c_str())) {
 		case 421:
-			return join(sender_prefix, code, target, ERR_UNKNOWNCOMMAND(message.command));
+			return join(sender_prefix, code, target, message);
 		case 461:
-			return join(sender_prefix, code, target, ERR_NEEDMOREPARAMS(message.command));
+			return join(sender_prefix, code, target, message);
 		case 462:
-			return join(sender_prefix, code, target, ERR_ALREADYREGISTRED());
+			return join(sender_prefix, code, target, message);
 		default:
 			return std::string();
 	}
