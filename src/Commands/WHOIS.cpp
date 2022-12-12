ecdfb59a899456b2e4d1203ddbbe6f843518b5f9
sichoi42 <42.4.sichoi@gmail.com>
2022-12-13 02:17:55 +0900

    :zap: improve WHOIS to treat multi inputs #31
    improve WHOIS to treat multi inputs #31

--- src/Commands/WHOIS.cpp
+++ src/Commands/WHOIS.cpp
@@ -1,14 +1,6 @@
 #include "../../include/Utils.hpp"
 
-std::string WHOIS(const Message &message, User *sender) {
-	std::string sender_prefix = sender->getServerPrefix();
-	Server *server = sender->getServer();
-
-	// nickname이 주어지지 않은 경우, 431 ERR_NONICKNAMEGIVEN
-	if (message.middle.size() < 1) {
-		return join(sender_prefix, "431", sender->getNickname(), ERR_NONICKNAMEGIVEN());
-	}
-	std::string nickname = message.middle[0];
+void	WHOISONE(std::string sender_prefix, std::string nickname, User *sender, Server *server) {
 	std::map<int, User *> users = server->getUsers();
 	for (std::map<int, User *>::iterator it = users.begin(); it != users.end(); it++) {
 		if (it->second->getNickname() == nickname) {
@@ -20,7 +12,7 @@ std::string WHOIS(const Message &message, User *sender) {
 
 			// 311 RPL_WHOISUSER
 			server->sendMsg(
-				join(sender_prefix, "311", nickname,
+				join(sender_prefix, "311", sender->getNickname(),
 				RPL_WHOISUSER(nickname, username, hostname, realname)
 				), sender);
 
@@ -32,7 +24,7 @@ std::string WHOIS(const Message &message, User *sender) {
 					channel_list += *it2 + " ";
 				}
 				server->sendMsg(
-					join(sender_prefix, "319", nickname,
+					join(sender_prefix, "319", sender->getNickname(),
 					RPL_WHOISCHANNELS(nickname, channel_list)
 					), sender);
 			}
@@ -42,38 +34,54 @@ std::string WHOIS(const Message &message, User *sender) {
 			+ std::string("Server started at ") + server->getStartTime();
 			// 312 RPL_WHOISSERVER
 			server->sendMsg(
-				join(sender_prefix, "312", nickname,
+				join(sender_prefix, "312", sender->getNickname(),
 				RPL_WHOISSERVER(nickname, server->getServername(), info)
 				), sender);
 
 			// 301 RPL_AWAY
 			server->sendMsg(
-				join(sender_prefix, "301", nickname,
-				RPL_AWAY(message.middle[0], "This is message from IRC server.")
+				join(sender_prefix, "301", sender->getNickname(),
+				RPL_AWAY(nickname, "This is message from IRC server.")
 				), sender);
 
 			// 313 RPL_WHOISOPERATOR
 			if (it->second->getMode() & FLAG_CHANNEL_O) {
 				server->sendMsg(
-					join(sender_prefix, "313", nickname,
+					join(sender_prefix, "313", sender->getNickname(),
 					RPL_WHOISOPERATOR(nickname)
 					), sender);
 			}
 			time_t cur_time = time(NULL);
 			// 317 RPL_WHOISIDLE
 			server->sendMsg(
-				join(sender_prefix, "317", nickname,
+				join(sender_prefix, "317", sender->getNickname(),
 				RPL_WHOISIDLE(nickname, static_cast<int>(difftime(cur_time, user->getLoginTime())))
 				), sender);
-
 			// 318 RPL_ENDOFWHOIS
 			server->sendMsg(
-				join(sender_prefix, "318", nickname,
+				join(sender_prefix, "318", sender->getNickname(),
 				RPL_ENDOFWHOIS(nickname)
 				), sender);
-			return std::string();
+		return ;
 		}
 	}
-	// nickname이 존재하지 않는 경우, 401 ERR_NOSUCHNICK
-	return join(sender_prefix, "401", nickname, ERR_NOSUCHNICK(nickname));
+	server->sendMsg(
+		join(sender_prefix, "401", sender->getNickname(),
+		ERR_NOSUCHNICK(nickname)
+		), sender);
+}
+
+std::string WHOIS(const Message &message, User *sender) {
+	std::string sender_prefix = sender->getServerPrefix();
+	Server *server = sender->getServer();
+
+	// nickname이 주어지지 않은 경우, 431 ERR_NONICKNAMEGIVEN
+	if (message.middle.size() < 1) {
+		return join(sender_prefix, "431", sender->getNickname(), ERR_NONICKNAMEGIVEN());
+	}
+	std::vector<std::string> nicknames = split(message.middle[0], ",");
+	for (std::vector<std::string>::iterator it = nicknames.begin(); it != nicknames.end(); it++) {
+		WHOISONE(sender_prefix, *it, sender, server);
+	}
+	return std::string();
 }
