e9c7d3efc546346f4135756d38b1181035f32c38
sichoi42 <42.4.sichoi@gmail.com>
2022-12-19 14:39:12 +0900

    :bug: fix bug on WHO #53
    fix bug on WHO #53

--- src/Commands/WHO.cpp
+++ src/Commands/WHO.cpp
@@ -1,6 +1,9 @@
 #include "../../include/Utils.hpp"
 
 bool isInSameChannel(const std::set<std::string> &channels, const std::set<std::string> &target_channels) {
+	if (channels.size() == 0 && target_channels.size() == 0) {
+		return false;
+	}
 	std::vector<std::string> common_channels;
 	std::set_intersection(channels.begin(), channels.end(),
 	target_channels.begin(), target_channels.end(),
@@ -11,37 +14,45 @@ bool isInSameChannel(const std::set<std::string> &channels, const std::set<std::
 	return false;
 }
 
+std::string joinedChannelToString(std::set<std::string> joined_channels) {
+	std::string result = "";
+	for (std::set<std::string>::iterator it = joined_channels.begin(); it != joined_channels.end(); ++it) {
+		result += *it + ", ";
+	}
+	if (result.length() > 0) {
+		result = result.substr(0, result.length() - 2);
+	}
+	return result;
+}
+
 std::string WHO(const Message &message, User *sender) {
 	std::string sender_prefix = sender->getServerPrefix();
 	Server *server = sender->getServer();
 
 	// name이 주어지지 않았거나 '0'이나 '*'로 주어진 경우 모든 visible한 리스트를 보여준다.
-	// 1. 서버에 존재하는 모든 채널을 하나씩 확인한다.
-	// 2. 채널에 속한 유저를 하나씩 확인한다.
-	// 3. sender와 공통 채널이 있는지를 확인한다.
-	// 4. sender와 공통 채널이 없고, +i user mode가 아니라면 visible한 유저이다.
-	// 5. visible한 유저의 정보를 RPL_WHOREPLY로 응답한다.
+	// sender와 공통 채널이 없고, +i user mode가 아니라면 visible한 유저이다.
+	// 1. 서버에 존재하는 모든 유저를 하나씩 확인한다.
+	// 2. +i mode인 유저, 채널에 가입하지 않은 유저 필터링
+	// 3. sender와 공통 채널인 유저 필터링
+	// 4. visible한 유저의 정보를 RPL_WHOREPLY로 응답한다.
 	if (message.middle.size() < 1 || (message.middle.size() == 1 && (message.middle[0] == "*" || message.middle[0] == "0"))) {
+		std::map<int, User *> users = server->getUsers();
 		std::map<std::string, Channel *> channels = server->getChannels();
-		for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); it++) {
-			Channel *channel = it->second;
-			std::set<int> user_fds = channel->getUsers();
-			for (std::set<int>::iterator it = user_fds.begin(); it != user_fds.end(); it++) {
-				User *target_user = server->getUserByFd(*it);
-				if (isInSameChannel(sender->getJoinedChannels(), target_user->getJoinedChannels())) {
-					continue;
-				}
-				if (target_user->getMode() & FLAG_USER_I) {
-					continue;
-				}
-				// 352 RPL_WHOREPLY
-				server->sendMsg(
-					join(sender_prefix, "352", sender->getNickname(),
-					RPL_WHOREPLY(channel->getChannelname(), target_user->getUsername(),
-											target_user->getHostname(), target_user->getServerPrefix(),
-											target_user->getNickname(), target_user->getRealname())
-					), sender);
+		for (std::map<int, User *>::iterator it = users.begin(); it != users.end(); it++) {
+			User *target_user = it->second;
+			std::set<std::string> target_joined_channels = target_user->getJoinedChannels();
+			if (target_user->getMode() & FLAG_USER_I) {
+				continue;
 			}
+			if (isInSameChannel(target_joined_channels, sender->getJoinedChannels())) {
+				continue;
+			}
+			server->sendMsg(
+						join(sender_prefix, "352", sender->getNickname(),
+						RPL_WHOREPLY(joinedChannelToString(target_joined_channels), target_user->getUsername(),
+												target_user->getHostname(), target_user->getServerPrefix(),
+												target_user->getNickname(), target_user->getRealname())
+						), sender);
 		}
 		// 315 RPL_ENDOFWHO
 		return join(sender_prefix, "315", sender->getNickname(), RPL_ENDOFWHO("*"));
